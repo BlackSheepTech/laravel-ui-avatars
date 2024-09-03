b6ba5db93e0e3ce80ff524d9a6328394e48d4161
Israel Pinheiro <israel.pinheiro@live.com>
2024-09-02 21:27:41 -0300

    chore: Reorganize composer.json, remove unused dependencies, add test and validation tools

---
+++ src/UiAvatarsServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BlackSheepTech\UiAvatars;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * @internal
+ */
+class UiAvatarsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any package services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/pulse.php', 'pulse'
+        );
+    }
+
+    /**
+     * Bootstrap any package services.
+     */
+    public function boot(): void
+    {
+
+        $this->registerPublishing();
+        $this->registerCommands();
+    }
+
+    /**
+     * Register the package's publishable resources.
+     */
+    protected function registerPublishing(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/ui-avatars.php' => config_path('ui-avatars.php'),
+            ], ['ui-avatars', 'ui-avatars-config']);
+        }
+    }
+
+    /**
+     * Register the package's commands.
+     */
+    protected function registerCommands(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                //
+            ]);
+        }
+    }
+}
